25ff8d3aa8119e419b6baf933551b094e0af1ad9
andreypfau <andreypfau@ton.org>
2025-05-15 22:17:07 +0900

    fix test
    Signed-off-by: andreypfau <andreypfau@ton.org>

--- tonapi-tl/test/PrivateKeyTest.kt
+++ tonapi-tl/test/PrivateKeyTest.kt
@@ -1,14 +1,13 @@
 package org.ton.kotlin.api.pk
 
 import io.ktor.util.*
-import org.ton.kotlin.tl.asByteString
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 
 class PrivateKeyTest {
     @Test
     fun `test creation PublicKey`() {
-        val privateKeyEd25519 = PrivateKeyEd25519("d53mOPj3+xx69TYJZ2LvzhxrNn32WBvt/ioV4Ha4gz8=".decodeBase64Bytes().asByteString())
+        val privateKeyEd25519 = PrivateKeyEd25519("d53mOPj3+xx69TYJZ2LvzhxrNn32WBvt/ioV4Ha4gz8=".decodeBase64Bytes())
         val publicKeyEd25519 = privateKeyEd25519.publicKey()
         assertContentEquals(
             "4745ede03eb4ef607843359c1f206d061a5632f68caa6f63021aa23b400950fd".hexToByteArray(),
