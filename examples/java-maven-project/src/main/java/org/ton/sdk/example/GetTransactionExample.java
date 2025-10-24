39d45f2d5e4fe43a677f1f0b6de8c2edca57350d
andreypfau <andreypfau@ton.org>
2025-10-24 23:57:13 +0900

    java maven example for ton-sdk-toncenter
    Signed-off-by: andreypfau <andreypfau@ton.org>

---
+++ examples/java-maven-project/src/main/java/org/ton/sdk/example/GetTransactionExample.java
@@ -0,0 +1,31 @@
+package org.ton.sdk.example;
+
+import org.ton.sdk.blockchain.address.AddressStd;
+import org.ton.sdk.blockchain.currency.Coins;
+import org.ton.sdk.toncenter.TonCenterV3Client;
+import org.ton.sdk.toncenter.model.TonCenterTransaction;
+import org.ton.sdk.toncenter.model.TonCenterTransactionsRequestBuilder;
+import org.ton.sdk.toncenter.model.TonCenterTransactionsResponse;
+
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+
+public class GetTransactionExample {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        TonCenterV3Client client = TonCenterV3Client.create();
+        TonCenterTransactionsResponse response = client.transactionsAsync(
+                new TonCenterTransactionsRequestBuilder()
+                        .address(AddressStd.parse("UQAKtVj024T9MfYaJzU1xnDAkf_GGbHNu-V2mgvyjTuP6uYH"))
+                        .limit(15)
+        ).get();
+        for (TonCenterTransaction transaction : response.getTransactions()) {
+            Coins balance = transaction.getAccountStateAfter().getBalance();
+
+            BigInteger value = BigInteger.ZERO;
+            if (balance != null) {
+                value = balance.getValue();
+            }
+            System.out.println("hash="+transaction.getHash()+" lt="+transaction.getLt()+" balance="+value);
+        }
+    }
+}
