20902ae131e3d8245ac076ad83090affbffdff07
andreypfau <andreypfau@ton.org>
2025-10-27 19:28:06 +0900

    disable examples-kotlin-gradle-project
    Signed-off-by: andreypfau <andreypfau@ton.org>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -39,7 +39,7 @@ submodule("liteclient")
 submodule("contract")
 submodule("dict")
 
-submodule("examples-kotlin-gradle-project", path = "examples/kotlin-gradle-project", "sdk")
+//submodule("examples-kotlin-gradle-project", path = "examples/kotlin-gradle-project", "sdk")
 
 //submodule("provider", group = "sdk")
 //submodule("provider-core", "provider/core", group = "sdk")
